8cf9dfb8171b3a0d31efc589c4569c118f0fedc6
Sanket Mathur <mathur.s@husky.neu.edu>
2018-01-28 17:05:58 -0500

    Added database

--- routes.py
+++ routes.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/mapapp'
+
 @app.route("/")
 def index():
 	return render_template("index.html")
